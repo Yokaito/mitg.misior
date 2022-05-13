5586dfe29869a44ff0fb61f825095772db79ca90
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2022-05-13 14:47:16 -0400

    feat: add migration otserver global

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,7 +2,33 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+const serverConfig = [
+  {
+    config: `db_version`,
+    value: `18`,
+  },
+  {
+    config: `motd_hash`,
+    value: ``,
+  },
+  {
+    config: `motd_num`,
+    value: `0`,
+  },
+  {
+    config: `players_record`,
+    value: `0`,
+  },
+];
+
 export const main = async (): Promise<void> => {
+  const configs = await prisma.server_config.findMany();
+  console.log(configs);
+  if (configs.length === 0) {
+    await prisma.server_config.createMany({
+      data: serverConfig,
+    });
+  }
   return;
 };
 
