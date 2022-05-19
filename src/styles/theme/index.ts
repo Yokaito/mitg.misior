3cba6f2ef0cc9a8e937960564264e04cda08fdbf
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2022-05-18 21:15:28 -0400

    feat: add new tokens in theme

--- src/styles/theme/index.ts
+++ src/styles/theme/index.ts
@@ -2,4 +2,19 @@ import { DefaultTheme } from 'styled-components';
 
 export const defaultTheme: DefaultTheme = {
   primary: `blue`,
+  defaultColors: {
+    black: `#000000`,
+    gray: `#3a3738`,
+    white: `#fff`,
+  },
+  textColors: {
+    primary: `#ffd698`,
+    secondary: `#f1fbec`,
+    tertiary: `#fede59`,
+    error: `#f44336`,
+  },
+  shadows: {
+    primary: `#2c2c2c`,
+    secondary: `#875f3e`,
+  },
 };
--- src/styles/theme/theme.d.ts
+++ src/styles/theme/theme.d.ts
@@ -3,5 +3,20 @@ import 'styled-components';
 declare module 'styled-components' {
   export interface DefaultTheme {
     primary: string;
+    defaultColors: {
+      black: string;
+      gray: string;
+      white: string;
+    };
+    textColors: {
+      primary: string;
+      secondary: string;
+      tertiary: string;
+      error: string;
+    };
+    shadows: {
+      primary: string;
+      secondary: string;
+    };
   }
 }
