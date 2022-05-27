4c5d2c232e731b5f5b56da353135d27a3f271548
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2022-05-27 12:25:22 -0400

    prisma: add new model newsticker

---
+++ prisma/migrations/20220527162321_add_newsticker/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `newsticker` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `message` MEDIUMTEXT NOT NULL,
+    `icon` VARCHAR(50) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
